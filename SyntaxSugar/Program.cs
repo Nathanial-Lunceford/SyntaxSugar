47b33598d1460e5363acbe3524800268469b2f59
Nate <nathanial.lunceford@gmail.com>
2022-07-18 10:46:13 -0500

    Initial Commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var answer = 4;
+        var response = "";
+
+        response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than " +
+            $"or equal to nine";
+
+
+        Console.WriteLine(response);
+    }
+}
